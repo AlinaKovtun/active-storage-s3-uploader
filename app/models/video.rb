7b15820ad352861aa5583ef62817f66482f4fb1a
Alina Kovtun <alinakovtun11@gmail.com>
2021-03-25 11:02:47 +0200

    Validate uploads

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,4 @@
 class Video < ApplicationRecord
   belongs_to :word
-  mount_uploader :attachment, AttachmentUploader, validate_download: false, validate_integrity: false, validate_processing: false
+  mount_uploader :attachment, AttachmentUploader
 end
